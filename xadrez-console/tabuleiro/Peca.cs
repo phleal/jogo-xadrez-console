20eee2138628180c0402950360e03f03289d4978
Phillip Leal <philliplealgs@gmail.com>
2022-05-28 20:19:23 -0300

    Fazendo os primeiros testes de movimentacao

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -17,5 +17,10 @@ namespace tabuleiro
             this.cor = cor;
             this.qteMovimentos = 0;
         }
+
+        public void incrementarQteMovimentos()
+        {
+            qteMovimentos++;
+        }
     }
 }
