df02b9418daef127aba9e2f6bbae538bdc36342a
Phillip Leal <philliplealgs@gmail.com>
2022-05-29 23:46:48 -0300

    Implementando a  verificacao de xeque

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -18,6 +18,10 @@ namespace tabuleiro
         {
             qteMovimentos++;
         }
+        public void decrementarQteMovimentos()
+        {
+            qteMovimentos--;
+        }
 
         public bool existeMovimentosPossiveis()
         {
